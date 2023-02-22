d76932267b06d485827411af392e6764ddf175b9
felixfavour <felixfavour0@gmail.com>
2023-02-22 10:48:24 +0100

    chore: added encryption to saved cookies

--- components/CookiesHome.vue
+++ components/CookiesHome.vue
@@ -5,11 +5,11 @@
       <form @submit.prevent>
         <label for="email">
           Email
-          <input v-model="email" type="email" name="" id="">
+          <input v-model="email" type="email" id="email">
         </label>
         <label for="password">
           Password
-          <input v-model="password" type="password" name="" id="">
+          <input v-model="password" type="password" id="pass">
         </label>
         <button :disabled="!(email.length > 4 && password.length > 1)" type="submit" @click="logIn">
           Log in
@@ -21,23 +21,40 @@
 
 <script setup>
   import Cookies from 'js-cookie'
+  import CryptoJS from 'crypto-js'
 
   const email = ref('')
   const password = ref('')
+  const config = useRuntimeConfig()
 
   const logIn = () => {
+    storeEncryptedCookie()
+    alert('Token generated')
+  }
+
+  const storeEncryptedCookie = () => {
     const inSixHours = 0.25
+    const randomJWT = generateRandomFakeJWT()
+
+    /**
+     * Encrypt JWT with encryptionKey from ENV variable
+     * [-NOTE-: Comment above is only used for educative purpose, should not be used similarly in practice]
+     * When an attacker knows where to look, information security is compromised by a step;
+     * With diligence from attacker, whole system can be easily compromised.
+     */
+    const encryptedRandomJWT = CryptoJS.AES.encrypt(randomJWT, config.encKey).toString()
+    console.log(encryptedRandomJWT)
+    
     /**
      * [secure] attribute set to true to ensure that cookie is inaccessible by unsecure(HTTP) domains/host
      * [sameSite] attribute set to 'Strict' to ensure that the client sends the cookie to only sites with similar domain/origin as itself.
      * [expires] attribute set to 0.25 (six hours) to ensure that Cookie is gone before attackers can break down all walls of protection.
      */
-    Cookies.set('jwt', generateRandomFakeJWT(), {
+    Cookies.set('jwt', encryptedRandomJWT, {
       secure: true,
       sameSite: 'Strict',
       expires: inSixHours
     })
-    alert('Token generated')
   }
 
   /**
